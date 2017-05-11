eff6ef48b38c810e39b7d68c960f41b94bb079cd
liubingfeng93@gmail.com <liubingfeng93@gmail.com>
2017-05-11 23:43:29 +1000

    skill modified to cope with rpc calls to show and apply

--- Visualization/skill.py
+++ Visualization/skill.py
@@ -8,6 +8,7 @@ class Skill(Image):
         self.skill_name = skill_name
         width, height = image.get_rect().size
         self.applied = applied
+        self.can_applied = False
         super().__init__(image, center_x, center_y, int(height*scale), int(width*scale))
 
 
@@ -24,13 +25,13 @@ class Skill(Image):
         skill_function = skill_name_handler[self.skill_name]
         skill_function(cur_villager, villagers_list, monster, player)
 
-    def animal_handler(self, villagers_list, monster, player):
+    def animal_handler(self,cur_villager, villagers_list, monster, player):
 
         if self.applied:
             return
 
-        for one_villager in villagers_list:
-            for one_tile in one_villager.land.tiles:
+        if self.can_applied:
+            for one_tile in cur_villager.land.tiles:
                 if one_tile.tile_type == Constant.TILE_TYPE_ANIMAL:
                     one_tile.display_plant_or_animal = True
                     self.applied = True
@@ -39,30 +40,37 @@ class Skill(Image):
 
         if self.applied:
             return
-
-        for one_villager in villagers_list:
-            one_villager.addItemToLeftHand(ConstantImage.ARMOUR_IMAGE_SPRITE,Constant.ITEM_NAME_ARMOUR ,Constant.ARMOUR_IMAGE_SCLAE)
+        if self.can_applied:
+            cur_villager.addItemToLeftHand(ConstantImage.ARMOUR_IMAGE_SPRITE,Constant.ITEM_NAME_ARMOUR ,Constant.ARMOUR_IMAGE_SCLAE)
             self.applied = True
             # one_villager.defend_power_increase(Constant.ITEM_ARMOUR_DEFEND_POWER_ADD)
 
 
-    def plant_handler(self, villagers_list, monster, player):
-        for one_villager in villagers_list:
-            for one_tile in one_villager.land.tiles:
+    def plant_handler(self,cur_villager ,villagers_list, monster, player):
+        if self.can_applied:
+            for one_tile in cur_villager.land.tiles:
                 if one_tile.tile_type == Constant.TILE_TYPE_PLANT:
                     one_tile.display_plant_or_animal = True
+                    self.applied = True
 
     def sword_handler(self, cur_villager,villagers_list, monsters, player):
-        for one_villager in villagers_list:
-            one_villager.addItemToRightHand(ConstantImage.ARMOUR_IMAGE_SPRITE,Constant.ITEM_NAME_ARMOUR ,Constant.ARMOUR_IMAGE_SCLAE)
+        if self.can_applied:
+            cur_villager.addItemToRightHand(ConstantImage.ARMOUR_IMAGE_SPRITE,Constant.ITEM_NAME_ARMOUR ,Constant.ARMOUR_IMAGE_SCLAE)
+            self.applied = True
             # one_villager.attack_power_increase(Constant.ITEM_NAME_SWORD_ATTACK_POWER_ADD)
 
     # housebuilder
     def house_handler(self, cur_villager,villagers_list, monsters, player):
-        for one_villager in villagers_list:
-            one_villager.addHouse()
+
+        if self.applied:
+            return
+
+        if self.can_applied:
+            cur_villager.addHouse()
+            self.applied = True
 
     def render(self, screen):
+        # if applied show full image with out transparency
         if self.applied:
             self.image.set_alpha(255)
             self.image_rect = screen.blit(self.image, (self.x - self.width//2, self.y - self.height//2))
