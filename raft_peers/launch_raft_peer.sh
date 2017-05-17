47dcf0ff8ac8dfbaad770c876caf4cfb4e9c9149
liubingfeng93@gmail.com <liubingfeng93@gmail.com>
2017-05-17 23:33:34 +1000

    update shell script

--- raft_peers/launch_raft_peer.sh
+++ raft_peers/launch_raft_peer.sh
@@ -3,7 +3,7 @@
 # check get two arguments
 if [ $# -eq 1 ] 
 then
-    echo "total peers need to launch => $2"
+    echo "total peers need to launch => $1"
 else
     echo "invalid argument => need one args -> total number of peers "
     exit 1
@@ -11,6 +11,6 @@ fi
 
 for i in $(seq $1)
 do
-    python3 raft_single.py peer$i $1 &
+    python3 raft_single.py peer$i $1 -v > /dev/null &
     echo "launching peer_$i done"
 done
